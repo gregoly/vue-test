f5b422e08e90343bccfb446bfcbe7757e4daa2b9
Grygorii Oliinyk <grigolydev@gmail.com>
2022-10-06 18:39:00 +0100

    center menu items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,9 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="d-md-none">
-        <b-nav-item href="#">Chicken</b-nav-item>
-        <b-nav-item href="#">Beef</b-nav-item>
-        <b-nav-item href="#">Sushi</b-nav-item>
+        <b-nav-item href="#" class="text-center">Chicken</b-nav-item>
+        <b-nav-item href="#" class="text-center">Beef</b-nav-item>
+        <b-nav-item href="#" class="text-center">Sushi</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
